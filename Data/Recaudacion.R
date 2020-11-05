6f79becdd49663b5c1f681e82362cccf8c7994fc
David Ortega <david.ortega.alfaro@gmail.com>
2020-11-04 22:10:42 -0600

    Before finding cuadro shcp magico

--- Data/Recaudacion.R
+++ Data/Recaudacion.R
@@ -29,4 +29,30 @@ View(rec_estat)
 estatal_2013<-listarecestat[[10]]
 View(estatal_2013)
 estatal_2019<-listarecestat[[16]]
+View(estatal_2019)
+
+
+#plotting total income 
+dates<-seq(as.Date("2012/1/1"), by = "month", length.out = length(rec_total[,1]))
+ingresototal<-as.numeric(rec_total[,1])
+ingresoiva<-as.numeric(rec_total[,7])
+ingresoisr<-as.numeric(rec_total[,3])
+hidrocarburos<-as.numeric(rec_total[,20])
+derechos<-as.numeric(rec_total[,19])
+#Recaudación es acumulada
+df<-data.table(Meses=dates, Recaudacion_Total=ingresototal,IVA=ingresoiva,ISR=ingresoisr,
+               Hidrocarburos=hidrocarburos, Derechos=derechos)
+#Recogiendo la recaudación total de los años
+df_rectotal<-subset(df, format.Date(Meses, "%m")=="12")
+
+
+
+library(ggplot2)
+# Basic line plot with points
+p_rectot<-ggplot(data=df_rectotal, aes(x=Meses, y=Recaudacion, group=1)) +
+        geom_line()+
+        geom_point()
+
+p_rectot + ggtitle("Recaudación Anual Total") +
+        xlab("Años") + ylab("Recaudación, MDP")
 
